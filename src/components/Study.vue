56c7819f5fab357926ebdce130fa50b3f4fb3798
dsdunn <dsdunnmusic@gmail.com>
2020-05-23 13:08:24 -0600

    Boilerplate -initial commit

---
+++ src/components/Study.vue
@@ -0,0 +1,18 @@
+<template>
+  <table>
+    <tr>
+      <th>
+        a study
+      </th>
+    </tr>
+  </table>
+</template>
+
+<script>
+  export default {
+    name: 'Study',
+    props: {
+      study: Object
+    }
+  }
+</script>
---
+++ src/components/StudyContainer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <Study />
+  </div>
+</template>
+
+<script>
+import Study from './Study';
+
+export default {
+  name: 'StudyList',
+  components: {
+    Study
+  },
+  props: {
+    studies: Array
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
